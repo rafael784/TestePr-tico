32eccf32af7b8f05924d33a767b6aee09f7f992c
Rafael José <rafael.silva@fastsolucoes.com.br>
2020-10-21 16:02:27 -0300

    primeiro commit

---
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
@@ -0,0 +1,130 @@
+﻿
+$(document).ready(function () {
+
+    let cpfField = $(this).find("#CPF");
+
+    let rawCPF = "";
+
+    cpfField.keyup(
+        (e) => {
+
+            if (e.key == "Backspace") {
+
+                cpfField.val("");
+
+                return true;
+
+            }
+
+            var cpf = cpfField.val();
+
+            if (!e.key.match(/[0-9]/)) {
+
+                //if (e.key.match(/(\.|\-)/)) cpfField.val(cpfField.val());
+
+                //alert("Formato inválido do CPF");
+
+                cpfField.val(cpf.substring(0, cpf.length - 1));
+
+            } else {
+
+                if (cpf.length == 3) {
+                    cpfField.val(cpf + ".");
+                } else if (cpf.length == 7) {
+                    cpfField.val(cpf + ".");
+                } else if (cpf.length == 11) {
+                    cpfField.val(cpf + "-");
+                }
+
+            }
+
+        }
+    );
+
+
+    $('#formCadastro').submit(function (e) {
+        e.preventDefault();
+
+        function validadorCPF(strCPF) {
+            var Soma;
+            var Resto;
+            Soma = 0;
+            strCPF = strCPF.replaceAll(".", "").replaceAll("-", "");
+            if (strCPF == "00000000000") return false;
+
+            for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+            Resto = (Soma * 10) % 11;
+
+            if ((Resto == 10) || (Resto == 11)) Resto = 0;
+            if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+            Soma = 0;
+            for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+            Resto = (Soma * 10) % 11;
+
+            if ((Resto == 10) || (Resto == 11)) Resto = 0;
+            if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+            return true;
+        }
+
+        if (!validadorCPF($(this).find("#CPF").val())) {
+            alert("CPF inválido");
+        } else {
+            $.ajax({
+                url: urlPost,
+                method: "POST",
+                data: {
+                    "NOME": $(this).find("#Nome").val(),
+                    "CEP": $(this).find("#CEP").val(),
+                    "Email": $(this).find("#Email").val(),
+                    "Sobrenome": $(this).find("#Sobrenome").val(),
+                    "Nacionalidade": $(this).find("#Nacionalidade").val(),
+                    "Estado": $(this).find("#Estado").val(),
+                    "Cidade": $(this).find("#Cidade").val(),
+                    "Logradouro": $(this).find("#Logradouro").val(),
+                    "Telefone": $(this).find("#Telefone").val(),
+                    "CPF": $(this).find("#CPF").val()
+                },
+                error:
+                    function (r) {
+                        if (r.status == 400)
+                            ModalDialog("Ocorreu um erro", r.responseJSON);
+                        else if (r.status == 500)
+                            ModalDialog("Ocorreu um erro", "Ocorreu um erro interno no servidor.");
+                    },
+                success:
+                    function (r) {
+                        ModalDialog("Sucesso!", r)
+                        $("#formCadastro")[0].reset();
+                    }
+            });
+        }
+
+
+    })
+
+})
+
+function ModalDialog(titulo, texto) {
+    var random = Math.random().toString().replace('.', '');
+    var texto = '<div id="' + random + '" class="modal fade">                                                               ' +
+        '        <div class="modal-dialog">                                                                                 ' +
+        '            <div class="modal-content">                                                                            ' +
+        '                <div class="modal-header">                                                                         ' +
+        '                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>         ' +
+        '                    <h4 class="modal-title">' + titulo + '</h4>                                                    ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-body">                                                                           ' +
+        '                    <p>' + texto + '</p>                                                                           ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-footer">                                                                         ' +
+        '                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>             ' +
+        '                                                                                                                   ' +
+        '                </div>                                                                                             ' +
+        '            </div><!-- /.modal-content -->                                                                         ' +
+        '  </div><!-- /.modal-dialog -->                                                                                    ' +
+        '</div> <!-- /.modal -->                                                                                        ';
+
+    $('body').append(texto);
+    $('#' + random).modal('show');
+}
